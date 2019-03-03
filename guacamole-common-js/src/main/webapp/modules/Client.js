1a48748cd0c5cccd646628e7eb9927a63ccb25bd
Virtually Nick <vnick@apache.org>
2019-03-03 18:29:01 -0500

    GUACAMOLE-221: Add stub handler for prompt instructions.

--- guacamole-common-js/src/main/webapp/modules/Client.js
+++ guacamole-common-js/src/main/webapp/modules/Client.js
@@ -1285,6 +1285,10 @@ Guacamole.Client = function(tunnel) {
             display.pop(layer);
 
         },
+        
+        "required": function receivePrompt(parameters) {
+            console.log('>>>PROMPT<<< Required: ' + parameters);
+        },
 
         "push": function(parameters) {
 
