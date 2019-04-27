7c39c90933497d7c64d4148ddda86c8ed4c828d9
Michael Barkdoll <mabarkdoll@gmail.com>
2019-04-26 19:01:21 -0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -17,4 +17,4 @@
 # under the License.
 #
 
-export CATALINA_OPTS="$CATALINA_OPTS -Djavax.net.debug=all -Djavax.net.ssl.trustStore=trustStore"
+export CATALINA_OPTS="$CATALINA_OPTS -Djavax.net.debug=all"
